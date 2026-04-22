fdfdf588b53cc67d965f293a2f2780af5eb30823
Vaishnav CH <vaishnavcherumanalil@gmail.com>
2026-04-22 20:48:50 +0530

    Remove mainnet bootstraps

--- ipfs/ipfs_manager.go
+++ ipfs/ipfs_manager.go
@@ -16,9 +16,7 @@ import (
 
 // Rubix MainNet bootstrap nodes
 var MainNetBootstrap = []string{
-	"/ip4/161.35.169.251/tcp/4001/p2p/12D3KooWPhZEYEw4jG3kSRuwgMEHcVt7KMkm1ui2ddu4fgSgwvDq",
-	"/ip4/103.127.158.120/tcp/4001/p2p/12D3KooWSQ94HRDzFf6W2rp7P8gzP6efZQHTaSU8uaQjskVBHiWP",
-	"/ip4/172.104.191.191/tcp/4001/p2p/12D3KooWFudnWZY1v1m4YXCzDWZSbNt7nvf5F42uzM6vErZ4NwqJ",
+	// Removed public mainnet bootstraps; Explorer acts as the bootstrap for the network
 }
 
 // Rubix TestNet bootstrap nodes
@@ -98,10 +96,10 @@ func getIPFSRepoPath() (string, error) {
 		return "", fmt.Errorf("failed to get current working dir: %w", err)
 	}
 	localPath := filepath.Join(cwd, "ipfs", ".ipfs")
-	
+
 	// Export it so that the bundled ipfs-linux binary actually reads it
 	os.Setenv("IPFS_PATH", localPath)
-	
+
 	return localPath, nil
 }
 
