5be8793ae959bfcb0d2f2b02e97cabf6200d7652
Vaishnav CH <vaishnavcherumanalil@gmail.com>
2026-04-24 17:42:48 +0530

    Fix

--- util/log_helper_linux.go
+++ util/log_helper_linux.go
@@ -7,9 +7,9 @@ import (
 	"syscall"
 )
 
-// RedirectStderr redirects the system-level stderr and stdout to the provided file.
+// RedirectStderr redirects the system-level stderr to the provided file.
 // This captures fatal Go runtime errors (like map races) that bypass the log package.
 func RedirectStderr(file *os.File) {
-	_ = syscall.Dup2(int(file.Fd()), int(os.Stdout.Fd()))
+	// Only redirect FD 2 (Stderr). Leave FD 1 (Stdout) alone so the user can see the terminal output.
 	_ = syscall.Dup2(int(file.Fd()), int(os.Stderr.Fd()))
 }
